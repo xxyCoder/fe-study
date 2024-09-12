031a5a0d00fe32338c2067f30f8a7049b97714fe
xxyCoder <1696944145@qq.com>
2024-09-12 20:22:34 +0800

    del

--- packing-tools/vite/source/zf/bin/www.js
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env node
-// 可以运行的脚本
-
-const createServer = require('../index.js')
-
-createServer().listen(5173, () => {
-  console.log('server start 5173 port', "http://localhost:5173")
-})
